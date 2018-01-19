8843582238092da56cf94c8fa12d38667f3d38f0
nathan-08 <nathanryan001@gmail.com>
2018-01-19 09:59:57 -0700

    begun work

--- competency-app/src/App.js
+++ competency-app/src/App.js
@@ -6,13 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Header H1</h1>
+        <div className= "classy-div">classy div</div>
+        <div id="superawesome-div">superawesome div
+          <div>
+            I am a child element. 
+          </div>
+          <section>
+            I am that ^ element's sibling
+              <p>
+                I am a descendent. 
+              </p>
+          </section>
+        </div>
+
       </div>
     );
   }
